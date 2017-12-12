9fab0836357371e9ab09dce9dc642f1c55a6e13c
Arne Brasseur <arne@arnebrasseur.net>
2017-12-12 19:39:35 +0100

    The version update script is used pre-commit, so it needs to git add its changes

--- autodoc.sh
+++ autodoc.sh
@@ -47,7 +47,7 @@ if ! git diff-index --quiet --cached HEAD ; then
     exit 1
 fi
 
-VERSION=0010
+VERSION=0012
 
 echo "//======================================\\\\"
 echo "||          AUTODOC v${VERSION}               ||"
--- update_version.sh
+++ update_version.sh
@@ -4,3 +4,4 @@
 
 cp autodoc.sh /tmp
 sed "s/VERSION=.*/$(printf "VERSION=%04d" `git rev-list --count HEAD`)/" /tmp/autodoc.sh > autodoc.sh
+git add autodoc.sh
